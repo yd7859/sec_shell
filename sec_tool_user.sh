d632ec098705ba20ca69fa218604d1461dcffe6d
yd7859 <87048969+yd7859@users.noreply.github.com>
2021-07-08 10:00:27 +0800

    Update sec_tool_user.sh

--- sec_tool_user.sh
+++ sec_tool_user.sh
@@ -5,12 +5,15 @@
 echo "/etc/passwd最后修改时间："
 ls -l /etc/passwd | cut -d " " -f 6-9
 
-echo "具有登录权限的用户"
-cat /etc/passwd | grep '/bin/bash'
+echo "具有登录权限的用户:"
+cat /etc/passwd | grep '/bin/bash'|cut -d ":" -f 1
 
-echo "空密码的用户"
+echo "系统中空口令账户:"
+awk -F: '($2=="!!") {print $1"账号空密码"}' /etc/shadow
+
+echo "具有登录权限且空密码的用户:"
 cat /etc/shadow |  cut -d ":" -f 1-3|grep ::| cut -d ":" -f 1
 
 
-echo "特权用户账号"
+echo "特权用户账号:"
 awk -F: '$3==0 {print $1}' /etc/passwd
