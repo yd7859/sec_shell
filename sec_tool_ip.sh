964cd03abfc9aca742c7d0adc2c4db5dd84082c0
yd7859 <87048969+yd7859@users.noreply.github.com>
2021-07-08 09:44:51 +0800

    Add files via upload

--- sec_tool_ip.sh
+++ sec_tool_ip.sh
@@ -4,12 +4,24 @@ echo "--------------------------------------------------------------------------
 #正常登录IP段
 sec_net=192.168.
 
-echo 当前登录用户信息
+echo 当前登录用户信息:
 who
 
-echo "最近系统登录用户 信息"
+echo "最近系统登录用户 信息:"
 last| grep -v `echo $sec_net`
 
-echo "尝试登录系统的ip "
-cat /var/log/audit/audit.log |grep failed |cut -d "=" -f 14|cut -d " " -f 1
+
+echo "用户错误登陆列表:"
+#cat /var/log/audit/audit.log |grep failed |cut -d "=" -f 14|cut -d " " -f 1
+
+lastb | head > /dev/null
+if [ $? == 1 ];then
+    echo ">>>无用户错误登入列表"
+else
+    echo ">>>用户错误登入"
+    lastb | head 
+fi
+
+echo "ssh错误尝试ip及次数:"
+    more /var/log/audit/audit.log|awk '/failed/{print $(NF-3)}'|sort|uniq -c|awk '{print ">>>登入失败的IP和尝试次数: "$2"="$1"次";}'
 
