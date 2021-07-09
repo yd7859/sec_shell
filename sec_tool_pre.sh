afe5da5159c3af4b3e83b143593c3a7c8863080c
yd7859 <87048969+yd7859@users.noreply.github.com>
2021-07-09 10:24:32 +0800

    Add files via upload

--- sec_tool_pre.sh
+++ sec_tool_pre.sh
@@ -1,9 +1,20 @@
-mkdir /var/local/sec
+if [ -d "/var/local/sec" ];
+then 
+    echo "目录已存在,更新配置文件。。。"
+else
+    mkdir /var/local/sec
+fi
+
 crontab -l >/var/local/sec/crontab_l.txt
 cat /etc/cron.daily/logrotate >/var/local/sec/crontab_daily.txt
-cat /etc/cron.hourly >/var/local/sec/crontab_hourly.txt
+ls -l /etc/cron.hourly >/var/local/sec/crontab_hourly.txt
 ls -l /etc/cron.weekly >/var/local/sec/crontab_weekly.txt
 ls -l /etc/cron.monthly >/var/local/sec/crontab_monthly.txt
 netstat -ntlup > /var/local/sec/port.txt
 cat /etc/rc.local >/var/local/sec/rc_local.txt
 ls -l /etc/rc3.d/ >/var/local/sec/rcxd.txt
+ls -l /bin > /var/local/sec/bin.txt
+ls -l /usr/bin > /var/local/sec/usrbin.txt
+ls -l /usr/sbin > /var/local/sec/usrsbin.txt
+
+	echo "配置文件更新完成。"
